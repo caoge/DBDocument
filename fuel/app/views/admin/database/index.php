b7287be331c5f79670c496a495044d02e3336263
blink <caogemail@163.com>
2013-11-28 01:42:18 +0800

    面包屑修改

--- fuel/app/views/admin/database/index.php
+++ fuel/app/views/admin/database/index.php
@@ -2,7 +2,7 @@
 	<ol class="breadcrumb">
 	  <li><a href="<?php echo Uri::create('admin/server'); ?>">服务器</a></li>
 	  <li><a href="<?php echo Uri::create('admin/database/index/'.$server_info['id']); ?>"><?php echo $server_info['name'] ?></a></li>
-	  <li class="active">数据库</li>
+	  <li class="active"></li>
 	</ol>
 	<div class="panel panel-default">
 		<div class="panel-heading">
